8aa439e50f2898beafa84dad76a47ae7b4c86b7b
n24chris05 <ChrisMacBookPro@Chriss-MacBook-Pro.local>
2013-11-18 17:46:05 +0800

    operation pressed

--- Calculator/Calculator/calculatorViewController.m
+++ Calculator/Calculator/calculatorViewController.m
@@ -9,21 +9,39 @@
 #import "calculatorViewController.h"
 
 @interface calculatorViewController ()
-
+@property (nonatomic) BOOL userIsInTheMiddelOfEnteringANumber;
 @end
 
 @implementation calculatorViewController
 @synthesize display = _display;
+@synthesize userIsInTheMiddelOfEnteringANumber = _userIsInTheMiddelOfEnteringANumber;
 
 - (IBAction)digitPressed:(UIButton *)sender {
+    /*
     NSString *digit = [sender currentTitle];
     UILabel *myDisplay = self.display; //getting the pointer to UILabel
     //Get the current text of UILabel
     NSString *currentText = [myDisplay text];
     //Append text
     NSString *newText = [currentText stringByAppendingString:digit];
-    [myDisplay setText:newText];
+    [myDisplay setText:newText]; //push new text to display
+     */
+    
+    NSString *digit = [sender currentTitle];
+    if (self.userIsInTheMiddelOfEnteringANumber){
+        self.display.text = [self.display.text stringByAppendingString:digit];
+    }else{
+    
+        self.display.text = digit;
+        self.userIsInTheMiddelOfEnteringANumber = YES;
+    }
+}
 
+- (IBAction)operationPressed:(UIButton *)sender {
+}
+
+- (IBAction)enterPressed {
+    
 }
 
 @end
