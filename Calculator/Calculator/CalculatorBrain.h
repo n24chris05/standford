06bdfd513daa02087cb7685f224ef8f740193688
n24chris05 <ChrisiMac@Chriss-iMac.local>
2013-11-18 20:52:29 +0800

    remodel

---
+++ Calculator/Calculator/CalculatorBrain.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by Chris Arquelada on 11/18/13.
+//  Copyright (c) 2013 Chris Arquelada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+@end
---
+++ Calculator/Calculator/CalculatorBrain.m
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.m
+//  Calculator
+//
+//  Created by Chris Arquelada on 11/18/13.
+//  Copyright (c) 2013 Chris Arquelada. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+
+@implementation CalculatorBrain
+
+@end
