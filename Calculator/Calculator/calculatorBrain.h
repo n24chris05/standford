454b61eea16388b8688b8abf5df6771695aacfc0
n24chris05 <ChrisMacBookPro@Chriss-MacBook-Pro.local>
2013-11-18 20:09:12 +0800

    calculator model

---
+++ Calculator/Calculator/calculatorBrain.h
@@ -0,0 +1,13 @@
+//
+//  calculatorBrain.h
+//  Calculator
+//
+//  Created by Chris Arquelada on 11/18/13.
+//  Copyright (c) 2013 Chris Arquelada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface calculatorBrain : NSObject
+
+@end
---
+++ Calculator/Calculator/calculatorBrain.m
@@ -0,0 +1,13 @@
+//
+//  calculatorBrain.m
+//  Calculator
+//
+//  Created by Chris Arquelada on 11/18/13.
+//  Copyright (c) 2013 Chris Arquelada. All rights reserved.
+//
+
+#import "calculatorBrain.h"
+
+@implementation calculatorBrain
+
+@end
