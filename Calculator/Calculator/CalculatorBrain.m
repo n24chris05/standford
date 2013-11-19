6bed6f7310db34d4ba90d682c5df0edbec757c01
n24chris05 <ChrisiMac@Chriss-iMac.local>
2013-11-20 00:27:01 +0800

    //using id

--- Calculator/Calculator/CalculatorBrain.m
+++ Calculator/Calculator/CalculatorBrain.m
@@ -26,31 +26,60 @@
 {
     [self.programStack addObject:[NSNumber numberWithDouble:operand]];
 }
--(double)popOperand
-{
-    NSNumber *operandObject = [self.operandStack lastObject];
-    if (operandObject)[self.operandStack removeLastObject];
-    return [operandObject doubleValue];
-}
+
 -(double)performOperation:(NSString *)operation
 {
     [self.programStack addObject:operation];
-    return [[CalculatorBrain runProgram:self.program]];
+    return [CalculatorBrain runProgram:self.program];
 }
+
+-(id)program
+{
+    return [self.programStack copy];
+}
+
++(NSString *)descriptionOfProgram:(id)program
+{
+    return @"Implement this in assignment 2";
+}
+
++(double)popOperandOffStack:(NSMutableArray *)stack
+{
     double result = 0;
     
-    if ([operation isEqualToString:@"+"]){
-        result = [self popOperand] + [self popOperand];
-    }else if ([@"*" isEqualToString:operation]){
-        result = [self popOperand] *[self popOperand];
-    }else if ([operation isEqualToString:@"-"]){
-        result = [self popOperand] - [self popOperand];
+    id topOfStack = [stack lastObject];
+    if (topOfStack)[stack removeLastObject];
+    
+    if ([topOfStack isKindOfClass:[NSNumber class] ]){
+        result = [topOfStack doubleValue];
+    }
+    else if ([topOfStack isKindOfClass:[NSString class]])
+    {
+        NSString *operation = topOfStack;
         
-    }else if ([operation isEqualToString:@"/"]){
+        if ([operation isEqualToString:@"+"]){
+            result = [self popOperandOffStack:stack] + [self popOperandOffStack:stack];
+        }else if ([@"*" isEqualToString:operation]){
+            result = [self popOperandOffStack:stack] *[self popOperandOffStack:stack];
+        }else if ([operation isEqualToString:@"-"]){
+            result = [self popOperandOffStack:stack] - [self popOperandOffStack:stack];
+            
+        }else if ([operation isEqualToString:@"/"]){
+            
+        }
     
     }
-        [self pushOperand:result];
     return result;
+}
+
++(double)runProgram:(id)program
+{
+    NSMutableArray *stack;
+    if ([program isKindOfClass:[NSArray class]]){
+    
+        stack = [program mutableCopy];
+    }
+    return [self popOperandOffStack:stack];
 }
 
 @end
